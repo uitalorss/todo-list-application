5298435557e789fd847eab99ebbb1d08475b4613
Uítalo <uitalo.urss@gmail.com>
2023-10-09 22:36:23 -0300

    ajuste de estilização na pagina de edição de tarefa

--- src/Pages/TaskDetails/styles.js
+++ src/Pages/TaskDetails/styles.js
@@ -6,8 +6,9 @@ export const TaskDetailContainer = styled(ListItem)`
   margin-top: 3rem;
   grid-template-columns: 1fr 600px 1fr 1fr;
   input {
+    font-size: 1rem;
     background: none;
     border: none;
-    color: #808080;
+    color: #f2f2f2;
   }
 `;
